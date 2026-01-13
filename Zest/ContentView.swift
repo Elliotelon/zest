c7d06c00941ce03daf6f02620b62b3ce37dafa52
minkyu <elliotelon@gmail.com>
2026-01-13 13:38:02 +0900

    Initial Commit

---
+++ Zest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zest
+//
+//  Created by 김민규 on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zest/ZestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZestApp.swift
+//  Zest
+//
+//  Created by 김민규 on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ZestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
