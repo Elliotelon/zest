f5727988b04ceef3951c92fdeba2137db414e9a7
minkyu <elliotelon@gmail.com>
2026-01-14 13:05:23 +0900

    feat: 환경별 빌드 구성 분리 및 보안 설정
    - xcconfig를 활용하여 개발(Dev)/릴리즈(Prod)/스테이징(Staging) 서버 주소 분리
    - .gitignore 설정을 통해 민감한 설정 파일(.xcconfig) 노출 방지
    - Info.plist 변수와 Env Helper를 통한 URL 동적 참조 구현
    - 협업을 위한 Config.sample 파일 추가

---
+++ Configs/Env.swift
@@ -0,0 +1,24 @@
+//
+//  Env.swift
+//  Zest
+//
+//  Created by 김민규 on 1/14/26.
+//
+
+import Foundation
+
+struct Env {
+    // Info.plist에서 ServerHost라는 키로 값을 읽어옵니다.
+    static let host: String = {
+        guard let host = Bundle.main.object(forInfoDictionaryKey: "ServerHost") as? String else {
+            // 설정을 실수했을 때 개발자가 바로 알 수 있게 에러 메시지 출력
+            fatalError("🚨 Info.plist에 'ServerHost' 설정이 누락되었습니다.")
+        }
+        return host
+    }()
+    
+    // 주소 완성 (https://를 여기서 붙여줌)
+    static var baseURL: String {
+        return "https://\(host)"
+    }
+}
