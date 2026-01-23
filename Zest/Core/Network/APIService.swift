ca0d0aa18eccd16a864f83a1866f4c49d916e5a4
minkyu <elliotelon@gmail.com>
2026-01-23 13:35:28 +0900

    fix: Ci 테스트 에러 해결을위해 임시로 테스트코드 전체 주석처리

--- Zest/Core/Network/APIService.swift
+++ Zest/Core/Network/APIService.swift
@@ -7,12 +7,19 @@
 
 import Foundation
 import Supabase
+import Auth
 
 final class APIService {
     
     static let shared = SupabaseClient(
             supabaseURL: Env.supabaseURL,
-            supabaseKey: Env.supabaseAnonKey
+            supabaseKey: Env.supabaseAnonKey,
+            options: SupabaseClientOptions(
+                auth: .init(
+                        emitLocalSessionAsInitialSession: true 
+                    )
+                )
+            
         )
         
     private init() {}
